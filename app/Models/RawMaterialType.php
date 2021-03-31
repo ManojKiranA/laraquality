a255aad5b5bd1220b6dc8f9d78341d6ac858fff7
sinan-aydogan <sinanaydogan@hotmail.com.tr>
2021-03-31 09:15:13 +0300

    Raw material relations added

--- app/Models/RawMaterialType.php
+++ app/Models/RawMaterialType.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class RawMaterialType extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','department_id','description','creator_id','updater_id '];
+
+    //RELATED DEPARTMENT
+    public function department()
+    {
+        return $this->belongsTo(Department::class,'department_id','id')->withDefault(['name' => '']);
+    }
 }
--- database/migrations/2021_01_13_174252_create_raw_materials_table.php
+++ database/migrations/2021_01_13_174252_create_raw_materials_table.php
@@ -18,12 +18,13 @@ class CreateRawMaterialsTable extends Migration
             $table->string('code',255);
             $table->string('name',255);
             $table->string('model',255)->nullable();
+            $table->string('manufacturer',255)->nullable();
             $table->string('description',750)->nullable();
-            $table->foreignId('manufacturer_id');
+            $table->foreignId('supplier_id');
             $table->foreignId('department_id');
             $table->foreignId('raw_material_type_id');
-            $table->json('package_type');
-            $table->json('stock_rules');
+            $table->json('package_type')->nullable();
+            $table->json('stock_rules')->nullable();
             $table->foreignId('creator_id');
             $table->foreignId('updater_id')->nullable();
             $table->softDeletes('deleted_at', 0);
